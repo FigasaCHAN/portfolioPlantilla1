3fb3abeef79020b587f6eb75e8c358872bcf24ab
FigasaCHAN <acostafa.dev@gmail.com>
2022-03-18 20:26:30 -0300

    Proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export function App(){
+  return (
+    <Fragment>
+      <h1> Hola mundo!</h1>
+      <h2> Pagina inicial</h2>
+    </Fragment>
+
+  )
+}
