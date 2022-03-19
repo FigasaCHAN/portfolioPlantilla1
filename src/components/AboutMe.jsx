c07e41f1520912e01ef85eb65e526e19e749c832
FigasaCHAN <acostafa.dev@gmail.com>
2022-03-19 15:20:49 -0300

    Changed: se cambio el nombre de una variable

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { TitleWithText } from './TitleWithText'
 
 import ImgSource from "../resource/img-source.json";
-const ILLUSTRATION = ImgSource['illustrations']["aboutMe"];
+const IMG_ABOUT_ME = ImgSource['illustrations']["aboutMe"];
 
 export function AboutMe() {
   return (
     <section className='about-me'>
         <h1> About Me </h1>
         <div className='container-about-me'> 
-            <img src={ILLUSTRATION} alt='' className='img-about-me'/>
+            <img src={IMG_ABOUT_ME} alt='' className='img-about-me'/>
             <div className='text-container-about-me'> 
                 <TitleWithText/>
                 <TitleWithText/>
