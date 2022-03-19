70a441cb113cf1725520e5b6f60ae21829cbfb88
FigasaCHAN <acostafa.dev@gmail.com>
2022-03-19 00:43:23 -0300

    Changed: Se cambia la fuente de las img

--- src/components/TextHeader.jsx
+++ src/components/TextHeader.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-//import {imgSource} from "../../resource/img-source.json"
-//const portada= imgSource['portada'];
+import React from 'react';
+import ImgSource from "../resource/img-source.json";
+const COVER_PHOTO = ImgSource['cover-photo'];
+
 export function TextHeader() {
   return (
     <section className='text-header'>
       <h1> Potencia tus ventas al menos a un 200% </h1>
-      <h2 style={{color:'blue'}}> Con una pagina web potente  </h2>
+      <h2 style={{color:'blue'}}> Con una pagina web potente {COVER_PHOTO} </h2>
+      <img src={COVER_PHOTO} alt="Cover-Photo"/>
+
       <div className="wave" style={{height: "150px", overflow: "hidden"}}>
                 <svg viewBox={"0 0 500 150"} preserveAspectRatio="none"
                 style={{height: "100%", width: "100%"}}>
